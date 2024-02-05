2b3f800caaf08da0f7941887b1f54157524c0a46
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-02-05 08:59:38 +0500

    auth

--- core/serializer.py
+++ core/serializer.py
@@ -3,10 +3,10 @@ from rest_framework import serializers
 from core.models import Category, Product, User
 
 
-class UserSerialzier(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['email', 'username', 'password']
 
 
 class CtgSerializer(serializers.ModelSerializer):
