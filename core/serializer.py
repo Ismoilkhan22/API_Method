cf8fc1217a4e60ac7db57349261f23ce74482e50
Ismoilkhan22 <143698453+Ismoilkhan22@users.noreply.github.com>
2024-02-05 13:50:12 +0500

    like

--- core/serializer.py
+++ core/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Category, Product, User
+from core.models import Category, Product, User, Like
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -19,3 +19,18 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class LikeSerializer(serializers.Serializer):
+    product = serializers.IntegerField()
+    like = serializers.BooleanField()
+
+    def create(self, validated_data):
+        like_obj, _ = Like.objects.get_or_create(product_id=validated_data['product'], user_id=self.context['request'].user.id)
+        if like_obj.is_like != validated_data['like']:
+            like_obj.is_like = validated_data['like']
+            like_obj.save()
+        return validated_data
+
+    def to_representation(self, instance):
+        return {"status": "Ok"}
